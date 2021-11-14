99d68ad29db1bd7fe4aaf8d2ff1b6a8d3c700476
Stelarys <93873622+Stelarys@users.noreply.github.com>
2021-11-14 18:28:27 +0200

    Add files via upload

---
+++ work.js
@@ -0,0 +1,4 @@
+const canvas = document.getElementById("Drawingplace");
+const toolbar = document.getElementById("  toolbar");
+const canvas = document.getElementById('Drawingplace')
+const ctx = canvas.getContext("2d");
